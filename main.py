c97e3a6f8cfdd9078bcf32692363b6cea859f7e6
NoiriK <noirik09@gmail.com>
2023-05-12 16:37:24 +0300

    edit mode

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class UserState:
     currentTeam = {}
     waitingForCounterPack = False
     waitingForPackToDelete = False
+    editor = False
 
     def __init__(self, currentTeam = {}):
         self.currentTeam = currentTeam
@@ -99,6 +100,24 @@ async def team_cmd(message: types.Message):
     except Exception as ex:
         await message.reply(ex)
 
+
+@dp.message_handler(commands=['toggle_edit_mode'])
+async def toggle_edit_mode_cmd(message: types.Message):
+    if message.from_user.username is None:
+        username = f'id@{message.from_user.id}'
+    else:
+        username = message.from_user.username
+
+    if username not in userStates:
+        userStates[username] = UserState()
+
+    if userStates[username].editor:
+        userStates[username].editor = False
+        await message.reply("Режим редактирования отключен")
+    else:
+        userStates[username].editor = True
+        await message.reply("Режим редактирования активирован")
+
 @dp.message_handler(commands=['help'])
 async def help_cmd(message: types.Message):
     await message.reply("""Бот выдает контрпаки по запросу, к примеру используйте команду /team и перечислите список персонажей через запятую, либо отделяя пробелом (имена в два слова в этом случае пишите слитно). Для каждого персонажа есть сокращения для удобного ввода.
@@ -144,13 +163,19 @@ async def processAddCounterPackRequest(message: types.Message, username):
                 storageEntity = storage.updateStorageEntity(storageEntity)
                 resultImg = ImageGenerator.generateImageForCharacterTeamRecord(storageEntity, recordToAdjust, username)
                 await bot.delete_message(message.chat.id, message.message_id)
-                await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                if userStates[username].editor:
+                    await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                else:
+                    await bot.send_photo(chat_id=message.chat.id, photo=resultImg)
             else:
                 await message.reply("Ошибка ввода, попробуйте еще раз")
         else:
             resultImg = ImageGenerator.generateImageForCharacterTeamRecord(storageEntity, recordToAdjust, username)
             await bot.delete_message(message.chat.id, message.message_id)
-            await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+            if userStates[username].editor:
+                await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+            else:
+                await bot.send_photo(chat_id=message.chat.id, photo=resultImg)
     else:
         await message.reply("Ошибка ввода, попробуйте еще раз")
 
@@ -170,7 +195,10 @@ async def processDeleteCounterPackRequest(message: types.Message, username):
                     storageEntity = storage.updateStorageEntity(storageEntity)
                     resultImg = ImageGenerator.generateImageForCharacterTeamRecord(storageEntity, recordToAdjust, username)
                     await bot.delete_message(message.chat.id, message.message_id)
-                    await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                    if userStates[username].editor:
+                        await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                    else:
+                        await bot.send_photo(chat_id=message.chat.id, photo=resultImg)
                 else:
                     await message.reply("Произощла ошибка, попробуйте еще раз")
             else:
@@ -191,18 +219,26 @@ async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.send_chat_action(chat_id=callback_query.message.chat.id, action="typing")
 
     if callback_query.data == "add":
-        if userStates[username].currentTeam != None:
-            userStates[username].waitingForPackToDelete = False
-            userStates[username].waitingForCounterPack = True
-            await callback_query.message.reply("Какой контрпак добавить?")
+        if userStates[username].editor:
+            if userStates[username].currentTeam != None:
+                userStates[username].waitingForPackToDelete = False
+                userStates[username].waitingForCounterPack = True
+                await callback_query.message.reply("Какой контрпак добавить?")
+            else:
+                logging.error(f'skipped callback touch')
         else:
-            logging.info(f'skipped callback touch')
+            await callback_query.message.reply("Режим редактирования отключен")
 
     elif callback_query.data == "delete":
-        if userStates[username].currentTeam is not None:
-            userStates[username].waitingForCounterPack = False
-            userStates[username].waitingForPackToDelete = True
-            await callback_query.message.reply("Какой контрпак удалить? (порядковый номер)")
+        if userStates[username].editor:
+            if userStates[username].currentTeam is not None:
+                userStates[username].waitingForCounterPack = False
+                userStates[username].waitingForPackToDelete = True
+                await callback_query.message.reply("Какой контрпак удалить? (порядковый номер)")
+            else:
+                logging.error(f'skipped callback touch')
+        else:
+            await callback_query.message.reply("Режим редактирования отключен")
 
     elif callback_query.data == "show":
         await showCharacterTeamForCurrentRequest(callback_query.message, username)
@@ -221,14 +257,20 @@ async def showCharacterTeamForCurrentRequest(message, username):
                     resultImg = ImageGenerator.generateImageForCharacterTeamRecord(storageEntity, recordToShow,
                                                                                    username)
                     await bot.delete_message(message.chat.id, message.message_id)
-                    await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                    if userStates[username].editor:
+                        await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                    else:
+                        await bot.send_photo(chat_id=message.chat.id, photo=resultImg)
                 else:
                     await message.reply("Произошла ошибка")
             else:
                 resultImg = ImageGenerator.generateImageForCharacterTeamRecord(storageEntity, recordToShow,
                                                                                username)
                 await bot.delete_message(message.chat.id, message.message_id)
-                await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                if userStates[username].editor:
+                    await bot.send_photo(chat_id=message.chat.id, photo=resultImg, reply_markup=inline_keyboard_second)
+                else:
+                    await bot.send_photo(chat_id=message.chat.id, photo=resultImg)
         else:
             await message.reply("Произошла ошибка")
     except Exception as ex:
